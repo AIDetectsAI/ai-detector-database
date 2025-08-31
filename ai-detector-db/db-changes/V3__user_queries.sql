22b29c22b23e69300770f2b8b790c2764fc3a1fd
jarzeckil <jarzeckil@gmail.com>
2025-08-31 15:59:28 +0200

    deleted default values

--- ai-detector-db/db-changes/V3__user_queries.sql
+++ ai-detector-db/db-changes/V3__user_queries.sql
@@ -1,9 +1,9 @@
 CREATE TABLE
     IF NOT EXISTS user_queries (
-        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        id UUID PRIMARY KEY,
         user_id UUID NOT NULL,
         image_path TEXT NOT NULL,
-        timestamp TIMESTAMP NOT NULL DEFAULT NOW (),
+        timestamp TIMESTAMP NOT NULL,
         result BOOLEAN NOT NULL,
         CONSTRAINT fk_user_queries_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
     );
