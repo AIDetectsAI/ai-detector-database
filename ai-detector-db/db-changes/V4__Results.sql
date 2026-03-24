48ffac523cf8ba48611adb4daa29bd44c3c676f6
Franek <betweter6@gmail.com>
2026-03-24 15:50:34 +0100

    results table, minor readme fix, .gitignore and formating fix

---
+++ ai-detector-db/db-changes/V4__Results.sql
@@ -0,0 +1,11 @@
+-- create result, create history (list of results), alter user
+CREATE TABLE IF NOT EXISTS results
+(
+    result_id   SERIAL PRIMARY KEY,
+    photo_id    UUID NOT NULL,
+    user_id     UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    model       VARCHAR(50) NOT NULL,
+    chance      NUMERIC(3, 1) NOT NULL,-- -99,9 to +99,9. Should be ok for majority of ways AI team can return the chance.
+    queried_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    is_deleted BOOLEAN DEFAULT FALSE
+);
