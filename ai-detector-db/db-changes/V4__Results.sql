8fa42fcb6747c7a309774a5026285f2230da90be
t-rogalski <tomaszrogalski04@gmail.com>
2026-04-15 19:56:33 +0200

    Result: url obrazka zamiast id

--- ai-detector-db/db-changes/V4__Results.sql
+++ ai-detector-db/db-changes/V4__Results.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS results
 (
     result_id   SERIAL PRIMARY KEY,
-    photo_id    UUID NOT NULL,
+    image_url   TEXT NOT NULL,
     user_id     UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     model       VARCHAR(50) NOT NULL,
     chance      NUMERIC(4, 2) NOT NULL,-- -99,99 to +99,99. Should be ok for majority of ways AI team can return the chance.
